e96d0e25d0d9ad9cbee2d3fb362b366885a35dc0
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-01-22 14:39:07 +0200

    Day 1 of 100 days of code challenge

---
+++ src/test/java/javafortesters/chap01/AClassWithAMethod.java
@@ -0,0 +1,13 @@
+package javafortesters.chap01;
+
+public class AClassWithAMethod {
+
+    public void aMethodOnAClass() {
+        System.out.println("Hello World");
+
+    }
+
+    public void aSecondMethodOnAClass() {
+        System.out.println("Second method is invoked");
+    }
+}
