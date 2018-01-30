c18e7e3b18d844c76a2d42857c88865d07c3885b
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-01-30 16:00:15 +0200

    Day 9 of 100 days of code challenge

---
+++ src/test/java/javafortesters/chap06/publicfields/User.java
@@ -0,0 +1,12 @@
+package javafortesters.chap06.publicfields;
+
+public class User {
+
+    public String username;
+    public String password;
+
+    public User() {
+        username = "admin";
+        password = "pA55w0rD";
+    }
+}
