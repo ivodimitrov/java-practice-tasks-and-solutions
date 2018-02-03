1b0c848066a753790d10457fa9b2049409a9eaa5
IvoDimitrov <ivopdimitrov@gmail.com>
2018-02-03 21:22:45 +0200

    Day 13 of 100 days of code challenge

--- src/test/java/javafortesters/chap02/FriendsContacts.java
+++ src/test/java/javafortesters/chap02/FriendsContacts.java
@@ -13,14 +13,15 @@ public class FriendsContacts {
                 "james@email.com", "0012223333");
 
         myContactsManager.addContact(friendJames);
-        // Create a new Contact object for Cezanne
+
         Contact friendCezanne = new Contact("Cezanne",
                 "cezanne@email.com", "987654321");
-        // Add Cezanne Contact to the ContactsManager
+
         myContactsManager.addContact(friendCezanne);
-        // Create a new Contact object for Jessica
-        Contact friendJessica = new Contact("Jessica", "jessica@email.com", "5554440001");
-        // Add Jessica Contact to the ContactsManager
+
+        Contact friendJessica = new Contact("Jessica",
+                "jessica@email.com", "5554440001");
+
         myContactsManager.addContact(friendJessica);
 
         // Search of a contact and display their phone number
