786bf19762c5c985bb04f8f836915174012dfa40
IvoDimitrov <ivopdimitrov@gmail.com>
2018-02-03 19:51:11 +0200

    Day 13 of 100 days of code challenge

---
+++ src/main/java/javafortesters/domainentities/Book.java
@@ -0,0 +1,26 @@
+package javafortesters.domainentities;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void borrowBook() {
+        isBorrowed = true;
+    }
+
+    public void returnBook() {
+        isBorrowed = false;
+    }
+
+    public boolean isBookBorrowed() {
+        return isBorrowed;
+    }
+
+}
