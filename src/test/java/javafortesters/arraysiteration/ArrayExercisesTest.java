3438d70cc7dfc2d4e8c08749b7ca43e3a3addcc3
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-02-08 14:20:42 +0200

    Day 18 of 100 days of code challenge

--- src/test/java/javafortesters/arraysiteration/ArrayExercisesTest.java
+++ src/test/java/javafortesters/arraysiteration/ArrayExercisesTest.java
@@ -10,64 +10,6 @@ import static org.junit.Assert.assertEquals;
 
 public class ArrayExercisesTest {
 
-    private void printSomeStars() {
-        System.out.println();
-        System.out.println("*********");
-    }
 
-    @Test
-    public void simpleArrayExample() {
-
-        String[] numbers0123 = {"zero", "one", "two", "three"};
-
-        printSomeStars();
-
-        for (String numberText : numbers0123) {
-            System.out.println(numberText);
-        }
-
-        assertEquals("zero", numbers0123[0]);
-        assertEquals("one", numbers0123[1]);
-    }
-
-    @Test
-    public void simpleCollectionExample() {
-
-        String[] numbers0123Array = {"zero", "one", "two", "three"};
-        List<String> numbers0123 = Arrays.asList(numbers0123Array);
-
-        printSomeStars();
-
-        for (String numberText : numbers0123) {
-            System.out.println(numberText);
-        }
-
-        assertEquals("zero", numbers0123.get(0));
-        assertEquals("one", numbers0123.get(1));
-        assertEquals("two", numbers0123.get(2));
-        assertEquals("three", numbers0123.get(3));
-    }
-
-    @Test
-    public void simpleDynamicCollectionExample() {
-
-        List<String> numbers0123 = new ArrayList<String>();
-
-        numbers0123.add("zero");
-        numbers0123.add("one");
-        numbers0123.add("two");
-        numbers0123.add("three");
-
-        printSomeStars();
-
-        for (String numberText : numbers0123) {
-            System.out.println(numberText);
-        }
-
-        assertEquals("zero", numbers0123.get(0));
-        assertEquals("one", numbers0123.get(1));
-        assertEquals("two", numbers0123.get(2));
-        assertEquals("three", numbers0123.get(3));
-    }
 }
 
