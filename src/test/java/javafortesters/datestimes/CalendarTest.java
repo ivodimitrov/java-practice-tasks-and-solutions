864e37b752cc399b648ba5c54a0e3d416b09f714
IvoDimitrov <ivopdimitrov@gmail.com>
2018-02-11 20:45:00 +0200

    Day 21 of 100 days of code challenge. Practise dates.

---
+++ src/test/java/javafortesters/datestimes/CalendarTest.java
@@ -0,0 +1,38 @@
+package javafortesters.datestimes;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalendarTest {
+
+    @Test
+    public void calendarExploration() {
+
+        Calendar cal = Calendar.getInstance();
+
+        System.out.println(cal.getTime().getTime());
+        System.out.println(System.currentTimeMillis());
+
+        System.out.println(cal.toString());
+
+        Calendar sameDate = Calendar.getInstance();
+        sameDate.setTime(cal.getTime());
+        assertThat(cal.equals(sameDate), is(true));
+        assertThat(cal.compareTo(sameDate), is(0));
+
+        System.out.println(cal.getTime().toString());
+        Calendar oneWeekFromNow = Calendar.getInstance();
+        oneWeekFromNow.setTime(cal.getTime());
+        oneWeekFromNow.add(Calendar.DATE, 7);
+        System.out.println(oneWeekFromNow.getTime().toString());
+
+        assertThat(oneWeekFromNow.after(cal), is(true));
+        assertThat(cal.before(oneWeekFromNow), is(true));
+        assertThat(cal.compareTo(oneWeekFromNow), is(-1));
+        assertThat(oneWeekFromNow.compareTo(cal), is(1));
+    }
+}
