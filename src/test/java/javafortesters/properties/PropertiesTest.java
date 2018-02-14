fb7e7f528ef4bf2253bc846751d01fa1eb634b1b
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-02-14 13:48:21 +0200

    Day 24 of 100 days of code challenge. Practise Properties class.

--- src/test/java/javafortesters/properties/PropertiesTest.java
+++ src/test/java/javafortesters/properties/PropertiesTest.java
@@ -1,4 +1,109 @@
 package javafortesters.properties;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
 public class PropertiesTest {
+
+    @Test
+    public void canCreateGetAndSetProperties() {
+
+        Properties properties = new Properties();
+
+        assertThat(properties.size(), is(0));
+
+        properties.setProperty("browser", "firefox");
+        properties.setProperty("port", "80");
+        assertThat(properties.getProperty("browser"), is("firefox"));
+        assertThat(properties.getProperty("browser"), is("firefox"));
+
+        assertThat(properties.getProperty("missing"), is(nullValue()));
+        assertThat(properties.getProperty("missing", "default"), is("default"));
+
+        assertThat(properties.containsKey("browser"), is(true));
+    }
+
+    @Test
+    public void canDisplayTheProperties() {
+
+        Properties properties = new Properties();
+        properties.setProperty("browser", "firefox");
+        properties.setProperty("port", "80");
+
+        assertThat(properties.stringPropertyNames().size(), is(2));
+
+        for (String key : properties.stringPropertyNames()) {
+            System.out.println("Key: " + key + " " +
+                    "Value: " + properties.getProperty(key));
+        }
+
+        properties.list(System.out);
+    }
+
+    @Test
+    public void canAccessSystemProperties() {
+
+        String workingDirectory = System.getProperty("user.dir");
+
+        String resourceFilePath = workingDirectory + "/src/test/resources/" +
+                "property_files/" +
+                "static_example.properties";
+
+        Properties sys = System.getProperties();
+        sys.list(System.out);
+    }
+
+    @Test
+    public void exampleSetWebdriverChromeIfNotSet() {
+        if (!System.getProperties().containsKey("webdriver.chrome.driver")) {
+            String currentDir = System.getProperty("user.dir");
+            String chromeDriverLocation
+                    = currentDir +
+                    "/../tools/chromedriver/chromedriver.exe";
+            System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
+        }
+    }
+
+    @Test
+    public void simpleSaveLoadPropertiesFile() throws IOException {
+
+        String tempDirectory = System.getProperty("java.io.tmpdir");
+        String tempResourceFilePath =
+                new File(tempDirectory,
+                        "tempFileForPropertiesStoreTest.properties")
+                        .getAbsolutePath();
+
+        Properties saved = new Properties();
+        saved.setProperty("prop1", "Hello");
+        saved.setProperty("prop2", "World");
+
+        FileOutputStream outputFile =
+                new FileOutputStream(tempResourceFilePath);
+        saved.store(outputFile, "Hello There World");
+        outputFile.close();
+
+        FileReader propertyFileReader =
+                new FileReader(tempResourceFilePath);
+        Properties loaded = new Properties();
+
+        try {
+            loaded.load(propertyFileReader);
+        } finally {
+            propertyFileReader.close();
+        }
+
+        assertThat(loaded.getProperty("prop1"), is("Hello"));
+        assertThat(loaded.getProperty("prop2"), is("World"));
+
+        new File(tempResourceFilePath).delete();
+    }
 }
