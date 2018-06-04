f0746308f6247f4088089d0809042b6331b5ac77
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-06-04 13:42:27 +0300

    Added comments.

--- src/main/java/firstheadjava/Cat.java
+++ src/main/java/firstheadjava/Cat.java
@@ -3,13 +3,13 @@ package firstheadjava;
 public class Cat extends Feline {
 
     /**
-     * 7HENWESAYhYOUMUSTIMPLEMENTTHEABSTRACTMETHODvTHATMEANSYOUMUST
-     * PROVIDEABODY4HATMEANSYOUMUSTCREATEANON
-     * ABSTRACTMETHODINYOURCLASS
-     * WITHTHESAMEMETHODSIGNATURENAMEANDARGUMENTS	ANDARETURNTYPETHATIS
-     * COMPATIBLEWITHTHEDECLAREDRETURNTYPEOFTHEABSTRACTMETHOD7HATYOUPUTIN
-     * THATMETHODISUPTOYOU!LL*AVACARESABOUTISTHATTHEMETHODISTHEREINYOUR
-     * CONCRETESUBCLASS
+     * When we say you must implement the abstract method that means
+     * you must provide a body. That means you must create a non
+     * abstract method in your class with the same method signature,
+     * name and arguments and a return type that is compatible with
+     * the declared return type of the abstract method that you put
+     * in that method is up to you. What java cares about is that
+     * the method is there in your concrete subclass.
      */
 
     @Override
