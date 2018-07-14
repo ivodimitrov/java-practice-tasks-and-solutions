e012536a0c204a1d0d54a6585795428f0e25e613
ivopdimitrov <ivopdimitrov@gmail.com>
2018-07-14 13:05:44 +0300

    Added CopyFile.

---
+++ src/main/java/javaabeginnersguide/practiseusingio/CopyFile.java
@@ -0,0 +1,30 @@
+package javaabeginnersguide.practiseusingio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyFile {
+    public static void main(String args[]) {
+        int i;
+
+        // First, confirm that both files has been specified.
+        if (args.length != 2) {
+            System.out.println("Usage: CopyFile from to");
+            return;
+        }
+
+        // Open and manage two files via the try statement.
+        try (FileInputStream fin = new FileInputStream(args[0]);
+             FileOutputStream fout = new FileOutputStream(args[1])) {
+
+            do {
+                i = fin.read();
+                if (i != -1) fout.write(i);
+            } while (i != -1);
+
+        } catch (IOException exc) {
+            System.out.println("I/O Error: " + exc);
+        }
+    }
+}
