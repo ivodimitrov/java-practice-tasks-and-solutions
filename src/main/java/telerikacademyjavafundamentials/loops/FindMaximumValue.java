ed5733ec68270fdce595b3fe50ba9ea3b7639e8f
Ivo Dimitrov <ivo.dimitrov@tide.co>
2019-01-03 18:41:38 +0200

    Part 2 of solutions for Loops contest

---
+++ src/main/java/telerikacademyjavafundamentials/loops/FindMaximumValue.java
@@ -0,0 +1,35 @@
+package telerikacademyjavafundamentials.loops;
+
+import java.util.Scanner;
+
+public class FindMaximumValue {
+  /*
+  Write a program that finds the maximum number from a given sequence.
+  Input
+  On the first line you will receive one number - N - the count of numbers to follow.
+  On the next N lines you will receive the sequence of numbers, each on a new line.
+  Output
+  Output only one number - the maximum number you find
+  Constraints
+  1 <= N <= 20
+  -200 <= each number <= 200
+   */
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
+    int numberOfValuesYouMustRead = in.nextInt();
+    double inputNumbers;
+    double max = Double.NEGATIVE_INFINITY;
+
+    for (int i = 0; i < numberOfValuesYouMustRead; i++) {
+      inputNumbers = in.nextDouble();
+
+      if (inputNumbers > max) {
+        max = inputNumbers;
+      }
+    }
+    System.out.printf("%.0f", max);
+  }
+}
---
+++ src/main/java/telerikacademyjavafundamentials/loops/MatrixNumbers.java
@@ -0,0 +1,34 @@
+package telerikacademyjavafundamentials.loops;
+
+import java.util.Scanner;
+
+public class MatrixNumbers {
+  /*
+  Write a program that reads from the console a positive integer number N and prints a matrix like in the examples below. Use two nested loops.
+  Input
+  The input will always consist of a single line, which contains the number N
+  Output
+  See the examples
+  Input
+  2
+  Output
+  Copy
+  1 2
+  2 3
+   */
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+
+    int number = in.nextInt();
+
+    for (int row = 1; row <= number; row++) {
+      for (int col = row; col <= row + number - 1; ) {
+        System.out.print(col + " ");
+        col++;
+      }
+      System.out.println();
+    }
+  }
+}
