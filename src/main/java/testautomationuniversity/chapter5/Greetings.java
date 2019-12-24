a902e04773f7b2e249bbf5efbc2e2a21fd1735aa
IvoDimitrov <ivopdimitrov@gmail.com>
2019-12-24 15:33:35 +0200

    Added Greetings

---
+++ src/main/java/testautomationuniversity/chapter5/Greetings.java
@@ -0,0 +1,20 @@
+package testautomationuniversity.chapter5;
+
+import java.util.Scanner;
+
+public class Greetings {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        scanner.close();
+        greetUser(name);
+    }
+
+    public static void greetUser(String name) {
+
+        System.out.println("Hi there, " + name);
+    }
+}
